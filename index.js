1644bee9e0ff686224abf9d3f2e35cdac3e8cc9d
Ivana <ivana@Ivanas-MacBook-Air.local>
2022-07-03 14:58:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('',(req, res, next) => {
+    res.send('I want more chocolate!');
+});
+
+app.get('/chocolate', (req, res, next) => {
+    let chocolates = [];
+    chocolates.push({name: 'Lindt'});
+    chocolates.push({name: 'Milka'});
+    chocolates.push({name: 'Dorina'});
+    res.json(chocolates);
+});
+
+app.listen(3000);
+
